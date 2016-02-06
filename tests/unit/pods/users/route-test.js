196941d135ee9244b09d08450242a9c47acc539b
Dominik Stodolny <dominik.stodolny@netguru.pl>
2016-02-07 00:17:58 +0100

    add users resource

---
+++ tests/unit/pods/users/route-test.js
@@ -0,0 +1,8 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:users', 'Unit | Route | users', {});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
