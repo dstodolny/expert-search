d524d21621a41f9345722c73d4f7990cd3e2ea80
Dominik Stodolny <dominik.stodolny@netguru.pl>
2016-02-13 00:05:52 +0100

    implement user's connection finder logic

---
+++ app/pods/user/controller.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+const { Controller } = Ember;
+
+export default Controller.extend({
+  options: Ember.computed('model', 'selected', function() {
+    return this.get('store').query('user', {
+      'user_id': this.get('model.id'),
+      'for_select': true
+    });
+  }),
+  isDisabled: Ember.computed.empty('selected'),
+  actions: {
+    userQueryChanged(query) {
+      const connections = [];
+      this.get('store').query('user', {
+        'user_id': this.get('model.id'),
+        'for_select': true,
+        q: query
+      }).then(users => {
+        connections.pushObjects(users.toArray());
+      });
+      return connections;
+    },
+    addConnection() {
+      const friendship = this.get('store').createRecord('friendship', {
+        userId: this.get('model.id'),
+        friendId: this.get('selected.id'),
+      });
+      friendship.save().then(() => {
+        this.set('selected', null);
+        this.store.findRecord('user', this.get('model.id'), { reload: true });
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+  }
+});
